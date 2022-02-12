cf000b883c1889d055bc4146a16bcea44dbbc639
dgplacenames <98712405+dgplacenames@users.noreply.github.com>
2022-02-12 22:23:37 +0000

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,8 +10,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Guess the Scottish place-name in 6 tries. After each guess, the colour of the tiles will
+        change to show how close your guess was to the place-name.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -22,7 +22,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter W is in the place-name and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -33,7 +33,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter L is in the place-name but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -44,7 +44,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell value="E" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter U is not in the place-name in any spot.
       </p>
     </BaseModal>
   )
