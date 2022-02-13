fc3b796df0484eafba7b41ec555a206666fe468d
dgplacenames <98712405+dgplacenames@users.noreply.github.com>
2022-02-13 22:13:52 +0000

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,10 +9,10 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
+        SPNle (Scottish Place-Name Wordle) is an open source version of the word guessing game we all know and
         love -{' '}
         <a
-          href="https://github.com/cwackerfuss/react-wordle"
+          href="https://github.com/dgplacenames/react-wordle"
           className="underline font-bold"
         >
           check out the code here
