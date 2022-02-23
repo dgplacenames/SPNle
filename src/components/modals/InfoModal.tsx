baf426b1804322178f8cd03f2fd8192e848a85ee
dgplacenames <98712405+dgplacenames@users.noreply.github.com>
2022-02-23 07:12:11 +0000

    Update InfoModal.tsx
    Updated from https://github.com/dgplacenames/DnG-le/blob/main/src/components/modals/InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -11,7 +11,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the Scottish place-name in 6 tries. After each guess, the colour of the tiles will
-        change to show how close your guess was to the place-name.
+        change to show how close your guess was to the place-name. (English words are accepted as guesses.)
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
@@ -45,6 +45,36 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter G is not in the place-name in any spot.
+      </p>
+    <p>&nbsp;</p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Never heard of the place? {' '}
+        <a
+          href="https://geo.nls.uk/maps/gb1900/#zoom=6&lat=55.0000&lon=-2.5000&layer=0"
+          className="underline font-bold"
+        >
+          Click here and type in the answer to see it on the map.
+        </a>{' '}
+      </p>
+      <p>&nbsp;</p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        This is an open source version of Wordle. {' '}
+        <a
+          href="https://github.com/dgplacenames/DnG-le"
+          className="underline font-bold"
+        >
+          Click here for the code and credits.
+        </a>{' '}
+      </p>
+      <p>&nbsp;</p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        {' '}
+        <a
+          href="https://twitter.com/dgplacenames"
+          className="underline font-bold"
+        >
+          @dgplacenames
+        </a>{' '}
       </p>
     </BaseModal>
   )
